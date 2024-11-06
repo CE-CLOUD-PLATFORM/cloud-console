facc10090a234ae4b19f4a165e9212e4147885ae
sirati hirunthani <bxdman@gmail.com>
2024-11-06 22:10:58 +0700

    migrate

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["cp.beget.com"], // Add the hostname here
-  },
-};
-
-export default nextConfig;
--- src/components/Sidemenu/InstanceMenu.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./style.css"
-const InstanceMenu = () => {
-  return (
-    <div className='side-menu'>InstanceMenu</div>
-  )
-}
-
-export default InstanceMenu
